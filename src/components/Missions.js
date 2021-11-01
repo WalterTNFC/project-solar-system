cc564e31e35aed46b11c143315fc88f1eee5a8e6
WalterCosta <156-201707140059@users.noreply.gitlab.lasse.ufpa.br>
2021-10-31 23:06:50 -0300

    Feito Requisito 7

---
+++ src/components/Missions.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        test
+      </div>
+    );
+  }
+}
+
+export default Missions;
